f5d584b6205bcec197adb55c7bd651748f783f66
Courtney Walker <ccwalkerjm@gmail.com>
2019-03-10 05:51:04 +0000

    update

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,100 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+const formatMoney = function (n, c, d, t) {
+    c = isNaN(c = Math.abs(c)) ? 2 : c;
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+    d = d === undefined ? "." : d;
+    t = t === undefined ? "," : t;
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+    const s = n < 0 ? "-" : "";
+    const i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c)));
+    //let j;
+    //j= (j = i.length) > 3 ? j % 3 : 0;
+    const j = 0;
+  return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 };
 
-app.initialize();
+
+const getStatus = function(x) {
+  if(x===0) return "fa-dove";
+  else if(x<25) return "md-face";
+  else return "fa-twitter";
+};
+
+
+function loadJson(url, callback) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      const json = JSON.parse(this.responseText);
+      callback(null, json);
+    }else {
+      callback(new Error(this.statusText));
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+
+const doInitPage= function(){
+  const populateTable = function(items){
+    const menu_items_elemt = document.querySelector('#menu-items');
+    menu_items_elemt.innerHTML = '<ons-row>'+
+                       '<ons-col width="5%"><h4>No</h4></ons-col>'+
+                       '<ons-col width="50%"><h4>Description</h4></ons-col>'+
+                       '<ons-col width="20%"><h4>Price</h4></ons-col>'+
+                       '<ons-col width="10%"><h4>Status</h4></ons-col>'+
+                       '<ons-col width="15%"></ons-col>'+
+                       '</ons-row>';
+    for(let i=0;i<items.length;i++){
+      const x = items[i];
+      const row_item =document.createElement("ons-row");
+
+      const col_no =document.createElement("ons-col");
+      col_no.setAttribute('width','5%');
+      col_no.innerHTML = '<span>'+(i+1).toString()+'</span>';
+      row_item.appendChild(col_no);
+
+      const col_description =document.createElement("ons-col");
+      col_description.setAttribute('width','50%');
+      col_description.innerHTML = '<span>'+x.menu+'</span>';
+      row_item.appendChild(col_description);
+
+      const col_price =document.createElement("ons-col");
+      col_price.setAttribute('width','20%');
+      col_price.innerHTML = '<span class="money">'+formatMoney(x.price)+'</span>';
+      row_item.appendChild(col_price);
+
+      const col_status =document.createElement("ons-col");
+      col_status.setAttribute('width','10%');
+      col_status.innerHTML = '<span><ons-icon icon="'+getStatus(x.status)+'"></ons-icon></span>';
+      row_item.appendChild(col_status);
+
+      const col_btn =document.createElement("ons-col");
+      col_btn.setAttribute('width','15%');
+      col_btn.setAttribute('class','btn_container');
+      const btn =document.createElement("ons-button");
+      btn.addEventListener('click', function(){
+        const msg = x.menu + " is " + (x.status === 0 ? "Finished" : (x.status < 25 ? "Almost Finised" : "OK"));
+        ons.notification.alert(msg);
+      });
+      btn.innerHTML = "Select";
+      col_btn.appendChild(btn);
+      row_item.appendChild(col_btn);
+
+      menu_items_elemt.appendChild(row_item);
+
+    }
+  };
+
+  let url = 'items.json';
+  loadJson(url, function(err, items){
+    if(err) return;
+    console.log("loading file", items);
+    populateTable(items);
+  });
+};
+
+
+ons.ready(function(){
+   doInitPage();
+});
